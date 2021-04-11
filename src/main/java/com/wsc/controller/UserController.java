8ecaad58a82f34d31c162a522c65724c1bbdf470
weishiba <185608130@qq.com>
2021-04-11 23:19:50 +0800

    配置thymeleaf模板引擎，测试页面

--- src/main/java/com/wsc/controller/UserController.java
+++ src/main/java/com/wsc/controller/UserController.java
@@ -3,17 +3,21 @@ package com.wsc.controller;
 import com.wsc.entity.User;
 import com.wsc.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 /**
  * @author 18560
  */
-@RestController
+@Controller
 @RequestMapping("/user")
 public class UserController {
 
@@ -21,17 +25,23 @@ public class UserController {
     private UserService userService;
 
     @GetMapping("/getUsers")
-    public List<User> getUsers(){
-        return userService.getAllUser();
+    public ModelAndView getUsers(HttpServletRequest request,Model model){
+        List<User> allUser = userService.getAllUser();
+        model.addAttribute("allUser",allUser);
+        model.addAttribute("title","所有用户");
+        return new ModelAndView("users/user","user",model);
     }
 
+
     @RequestMapping("/getuser/{id}")
+    @ResponseBody
     public String getuser(@PathVariable long id){
         System.out.println("111111111111");
         return userService.getUser(id).toString();
     }
 
     @RequestMapping("/get")
+    @ResponseBody
     public User getuser(){
         System.out.println("111111111111");
         User user = new User();
@@ -39,5 +49,10 @@ public class UserController {
         user.setPassword("11111");
         System.out.println("11111");
         return user;
+    }
+
+    @RequestMapping("/empty")
+    public String empty() {
+        return "users/empty";
     }
 }
